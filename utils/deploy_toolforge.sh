585154b2b67867048867827bff48b65cdfd397aa
David Caro <dcaro@wikimedia.org>
2022-01-13 12:19:33 +0100

    utils: Added setup and deploy scripts
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- utils/deploy_toolforge.sh
+++ utils/deploy_toolforge.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-ln -s $PWD/wm_what/static .
-ln -s $PWD/wm_what/templates .
-ln -s $PWD/wm_what/app.py .
-ln -s $HOME/prod-config.yaml .
+if [[ "$1" == "in_pod" ]]; then
+    source $HOME/www/python/venv/bin/activate
+    pip install -e wm-what/.
+else
+    cd wm-what
+    git fetch --all
+    git reset --hard origin/main
+    webservice --backend=kubernetes python3.9 shell -- bash ./deploy_toolforge.sh in_pod || true
+    webservice --backend=kubernetes python3.9 restart
+fi
---
+++ utils/setup_toolforge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $PWD/wm_what/static .
+ln -s $PWD/wm_what/templates .
+ln -s $PWD/wm_what/app.py .
+ln -s $HOME/prod-config.yaml .
+ln -s $PWD/utils/deploy_toolforge.sh $HOME/deploy_toolforge.sh
